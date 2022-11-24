52b4c6d0083acbd33b3494a8a06e3edf2ce7ebff
WangWen-Believer <1204746392@qq.com>
2022-11-24 15:56:38 +0800

    change pcl version

--- docker/build/installers/install_pcl.sh
+++ docker/build/installers/install_pcl.sh
@@ -30,14 +30,15 @@ CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 #   apt-get -y install \
 #   libpcl-dev
 # exit 0
-if ldconfig -p | grep -q libpcl_common ; then
-    info "Found existing PCL installation. Skipp re-installation."
-    exit 0
-fi
+#if ldconfig -p | grep -q libpcl_common ; then
+#    info "Found existing PCL installation. Skipp re-installation."
+#    exit 0
+#fi
 
 GPU_OPTIONS="-DCUDA_ARCH_BIN=\"${SUPPORTED_NVIDIA_SMS}\""
 if [ "${WORKHORSE}" = "cpu" ]; then
     GPU_OPTIONS="-DWITH_CUDA=OFF"
+    echo "GPU_OPTIONS="-DWITH_CUDA=OFF""
 fi
 
 info "GPU Options for PCL:\"${GPU_OPTIONS}\""
@@ -45,9 +46,12 @@ info "GPU Options for PCL:\"${GPU_OPTIONS}\""
 TARGET_ARCH="$(uname -m)"
 ARCH_OPTIONS=""
 if [ "${TARGET_ARCH}" = "x86_64" ]; then
+    
     ARCH_OPTIONS="-DPCL_ENABLE_SSE=ON"
+    echo "ARCH_OPTIONS="-DPCL_ENABLE_SSE=ON"
 else
     ARCH_OPTIONS="-DPCL_ENABLE_SSE=OFF"
+    echo "ARCH_OPTIONS="-DPCL_ENABLE_SSE=OFF"
 fi
 
 # libpcap-dev
@@ -77,11 +81,11 @@ apt_get_update_and_install \
 
 THREAD_NUM=$(nproc)
 
-# VERSION="1.11.0"
-# CHECKSUM="4255c3d3572e9774b5a1dccc235711b7a723197b79430ef539c2044e9ce65954" # 1.11.0
+ VERSION="1.11.0"
+ CHECKSUM="4255c3d3572e9774b5a1dccc235711b7a723197b79430ef539c2044e9ce65954" # 1.11.0
 
-VERSION="1.10.1"
-CHECKSUM="61ec734ec7c786c628491844b46f9624958c360012c173bbc993c5ff88b4900e" # 1.10.1
+#VERSION="1.11.1"
+#CHECKSUM="479f84f2c658a6319b78271111251b4c2d6cf07643421b66bbc351d9bed0ae93" # 1.11.1
 PKG_NAME="pcl-${VERSION}.tar.gz"
 
 DOWNLOAD_LINK="https://github.com/PointCloudLibrary/pcl/archive/${PKG_NAME}"
@@ -94,7 +98,7 @@ tar xzf ${PKG_NAME}
 #  -DCMAKE_SKIP_RPATH=ON \
 
 pushd pcl-pcl-${VERSION}/
-    patch -p1 < ${CURR_DIR}/pcl-sse-fix-${VERSION}.patch
+    #patch -p1 < ${CURR_DIR}/pcl-sse-fix-${VERSION}.patch
     mkdir build && cd build
     cmake .. \
         "${GPU_OPTIONS}" \
@@ -118,29 +122,29 @@ ldconfig
 ok "Successfully installed PCL ${VERSION}"
 
 # Clean up
-rm -fr ${PKG_NAME} pcl-pcl-${VERSION}
+#rm -fr ${PKG_NAME} pcl-pcl-${VERSION}
 
-if [[ -n "${CLEAN_DEPS}" ]]; then
-    # Remove build-deps for PCL
-    # Note(storypku):
-    # Please keep libflann-dev as it was required by local_config_pcl
-    apt_get_remove \
-        libeigen3-dev \
-        libglew-dev \
-        libglfw3-dev \
-        freeglut3-dev \
-        libusb-1.0-0-dev \
-        libdouble-conversion-dev \
-        libopenni-dev \
-        libjpeg-dev \
-        libpng-dev \
-        libtiff-dev \
-        liblz4-dev \
-        libfreetype6-dev \
-        libpcap-dev \
-        libqhull-dev
+#if [[ -n "${CLEAN_DEPS}" ]]; then
+#    # Remove build-deps for PCL
+#    # Note(storypku):
+#    # Please keep libflann-dev as it was required by local_config_pcl
+#    apt_get_remove \
+#        libeigen3-dev \
+#        libglew-dev \
+#        libglfw3-dev \
+#        freeglut3-dev \
+#        libusb-1.0-0-dev \
+#        libdouble-conversion-dev \
+#        libopenni-dev \
+#        libjpeg-dev \
+#        libpng-dev \
+#        libtiff-dev \
+#        liblz4-dev \
+#        libfreetype6-dev \
+#        libpcap-dev \
+#        libqhull-dev
 
-    # Add runtime-deps for pcl
+#    # Add runtime-deps for pcl
     apt_get_update_and_install \
         libusb-1.0-0 \
         libopenni0 \
@@ -149,8 +153,8 @@ if [[ -n "${CLEAN_DEPS}" ]]; then
         libdouble-conversion1 \
         libpcap0.8 \
         libqhull7
-fi
+#fi
 
-# Clean up cache to reduce layer size.
-apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+## Clean up cache to reduce layer size.
+#apt-get clean && \
+#    rm -rf /var/lib/apt/lists/*
