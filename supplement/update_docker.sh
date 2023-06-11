1a49b6338c9ecfa9094e559694d465e3116f82ee
ChenJiahao031008 <ChenJiahao031008@outlook.com>
2023-06-11 23:55:54 +0800

    [fix] fix bug about cyber_visualizer

--- supplement/update_docker.sh
+++ supplement/update_docker.sh
@@ -1,8 +1,40 @@
+set -e #错误即退出
+
 sudo cp /apollo/supplement/sources.list /etc/apt/
 sudo apt update
 
+function apt_get_update_and_install() {
+    # --fix-missing
+    apt-get -y update && \
+        apt-get -y install --no-install-recommends "$@"
+}
+
 sudo apt-get install tcl-dev tk-dev python3-tk libeigen3-dev -y
 sudo apt-get install liblapack-dev libsuitesparse-dev libcxsparse3 libgflags-dev libgtest-dev libglew-dev libcholmod3 libcxsparse3 -y
+apt_get_update_and_install \
+    libeigen3-dev \
+    libflann-dev \
+    libglew-dev \
+    libglfw3-dev \
+    freeglut3-dev \
+    libusb-1.0-0-dev \
+    libdouble-conversion-dev \
+    libopenni-dev \
+    libjpeg-dev \
+    libpng-dev \
+    libtiff-dev \
+    liblz4-dev \
+    libfreetype6-dev \
+    libpcap-dev \
+    libqhull-dev
+apt_get_update_and_install \
+    libusb-1.0-0 \
+    libopenni0 \
+    libfreetype6 \
+    libtiff5 \
+    libdouble-conversion1 \
+    libpcap0.8 \
+    libqhull7
 
 cd /apollo/third_party/pcl/
 FILE=/apollo/third_party/pcl/pcl-pcl-1.11.0
